ad38ff6b87825836948607f917c45bd88ed92768
kkocdko <kkocdko@163.com>
2022-05-19 00:58:34 +0800

    `units::qqbot` websocket

--- src/units/qqbot/mod.rs
+++ src/units/qqbot/mod.rs
@@ -1,39 +1,25 @@
 use crate::db;
 use crate::ticker::Ticker;
-use axum::extract::Json;
-use axum::response::IntoResponse;
+use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
 use axum::routing::MethodRouter;
 use axum::Router;
+use futures_util::{SinkExt, StreamExt};
 use once_cell::sync::Lazy;
-use serde::Deserialize;
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::Arc;
 use std::time::SystemTime;
-
-static REPLIES: Lazy<Mutex<HashMap<String, String>>> = Lazy::new(|| {
-    Mutex::new(HashMap::from([
-        ("呜".into(), "呜".into()),
-        ("你说对吧".into(), "啊对对对".into()),
-        ("运行平台".into(), "ksite / axum / mirai-go".into()),
-    ]))
-});
+use tokio::sync::broadcast::{self, Sender};
+use tokio::sync::Mutex;
 
 fn db_init() {
     db!("CREATE TABLE qqbot_groups (group_id INTEGER)").ok();
 }
-fn db_groups_get() -> Vec<u64> {
+fn db_groups_get() -> Vec<i64> {
     let result = db!("SELECT * FROM qqbot_groups", [], (0));
     result.unwrap().into_iter().map(|r| r.0).collect()
 }
-fn db_groups_insert(group_id: u64) {
+fn db_groups_insert(group_id: i64) {
     db!("INSERT INTO qqbot_groups VALUES (?)", [group_id]).unwrap();
-}
-
-fn elapse(time: f64) -> f64 {
-    // javascript: new Date("2001.01.01 06:00").getTime()
-    let epoch = SystemTime::UNIX_EPOCH;
-    let now = SystemTime::now().duration_since(epoch).unwrap().as_millis() as f64;
-    (now - time) / 864e5
 }
 
 async fn fetch_text(url: &str) -> String {
@@ -43,18 +29,36 @@ async fn fetch_json(url: &str) -> serde_json::Value {
     serde_json::from_str(&fetch_text(url).await).unwrap()
 }
 
-#[derive(Deserialize)]
-struct Event {
-    self_id: Option<i64>,
-    raw_message: Option<String>,
+fn elapse(time: f64) -> f64 {
+    // javascript: new Date("2001.01.01 06:00").getTime()
+    let epoch = SystemTime::UNIX_EPOCH;
+    let now = SystemTime::now().duration_since(epoch).unwrap().as_millis() as f64;
+    (now - time) / 864e5
 }
-async fn post_handler(Json(event): Json<Event>) -> impl IntoResponse {
-    let prefix = format!("[CQ:at,qq={}]", &event.self_id.unwrap_or_default());
-    let msg: Vec<&str> = match &event.raw_message {
-        Some(v) if v.starts_with(&prefix) => v.split(' ').skip(1).collect(),
-        _ => return Default::default(),
-    };
-    let reply = |v| format!(r#"{{ "reply": "[BOT] {v}" }}"#);
+
+fn op_send_group_msg(group_id: i64, msg: &str) -> String {
+    serde_json::json!({
+        "action": "send_group_msg",
+        "params": { "group_id": group_id, "message": msg }
+    })
+    .to_string()
+}
+
+static REPLIES: Lazy<Mutex<HashMap<String, String>>> = Lazy::new(|| {
+    Mutex::new(HashMap::from([
+        ("呜".into(), "呜".into()),
+        ("你说对吧".into(), "啊对对对".into()),
+        ("运行平台".into(), "ksite / axum / mirai-go".into()),
+    ]))
+});
+
+async fn event_handler(event: serde_json::Value) -> Option<String> {
+    let self_id = event.get("self_id")?.as_i64()?;
+    let group_id = event.get("group_id")?.as_i64()?;
+    let raw_message = event.get("raw_message")?.as_str()?;
+    let msg = raw_message.strip_prefix(&format!("[CQ:at,qq={self_id}]"))?;
+    let msg: Vec<&str> = msg.trim().split(' ').collect();
+    let reply = |v| Some(op_send_group_msg(group_id, v));
     match msg[0] {
         "乌克兰" | "俄罗斯" | "俄乌" => reply("嘘！"),
         "kk单身多久了" => reply(&format!("kk已连续单身 {:.3} 天了", elapse(10485432e5))),
@@ -92,29 +96,61 @@ async fn post_handler(Json(event): Json<Event>) -> impl IntoResponse {
             reply(r["data"]["info"]["text"].as_str().unwrap())
         }
         "订阅通知" => {
-            let group_id = msg[1].parse().unwrap();
             db_groups_insert(group_id);
             reply("订阅成功")
         }
         "设置回复" => {
             let (k, v) = (msg[1].into(), msg[2].into());
-            REPLIES.lock().unwrap().insert(k, v);
+            REPLIES.lock().await.insert(k, v);
             reply("记住啦")
         }
-        k if REPLIES.lock().unwrap().contains_key(k) => reply(&REPLIES.lock().unwrap()[k]),
+        k if REPLIES.lock().await.contains_key(k) => reply(&REPLIES.lock().await[k]),
         _ => reply("未知指令"),
     }
 }
 
+static BROADCAST: Lazy<Sender<String>> = Lazy::new(|| broadcast::channel(16).0);
+
+async fn ws_handler(ws: WebSocket) {
+    let (sender, mut receiver) = ws.split();
+    let sender = Arc::new(Mutex::new(sender));
+    let mut task1 = tokio::spawn({
+        let sender = sender.clone();
+        let mut broadcast = BROADCAST.subscribe();
+        async move {
+            while let Ok(v) = broadcast.recv().await {
+                if sender.lock().await.send(Message::Text(v)).await.is_err() {
+                    break;
+                }
+            }
+        }
+    });
+    let mut task2 = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(event))) = receiver.next().await {
+            if let Some(v) = event_handler(serde_json::from_str(&event).unwrap()).await {
+                if sender.lock().await.send(Message::Text(v)).await.is_err() {
+                    break;
+                }
+            }
+        }
+    });
+    tokio::select! {
+        _ = (&mut task1) => task2.abort(),
+        _ = (&mut task2) => task1.abort(),
+    };
+}
+
 pub fn service() -> Router {
     db_init();
-    Router::new().route("/qqbot", MethodRouter::new().post(post_handler))
+    Router::new().route(
+        "/qqbot",
+        MethodRouter::new().get(|u: WebSocketUpgrade| async { u.on_upgrade(ws_handler) }),
+    )
 }
 
 async fn notify(msg: &str) {
-    for id in db_groups_get() {
-        let url = format!("http://127.0.0.1:5700/send_group_msg?group_id={id}&message={msg}");
-        reqwest::get(url).await.unwrap();
+    for group_id in db_groups_get() {
+        BROADCAST.send(op_send_group_msg(group_id, msg)).ok();
     }
 }
 
@@ -138,12 +174,12 @@ impl UpNotify {
     async fn trigger(&self) {
         let v = Self::query(self.pkg_id).await;
         let changed = {
-            let last = self.last.lock().unwrap();
+            let last = self.last.lock().await;
             !last.is_empty() && *last != v
         };
         if changed {
             notify(&format!("{} {v} released!", self.name)).await;
-            *self.last.lock().unwrap() = v;
+            *self.last.lock().await = v;
         }
     }
 
@@ -159,7 +195,7 @@ impl UpNotify {
 static TICKER: Lazy<Mutex<Ticker>> =
     Lazy::new(|| Mutex::new(Ticker::new_p8(&[(-1, 20, 0), (-1, 50, 0)])));
 pub async fn tick() {
-    if !TICKER.lock().unwrap().tick() {
+    if !TICKER.lock().await.tick() {
         return;
     }
 
