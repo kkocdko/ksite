6594fc533265672e52c984fb0db65ad8f3adb920
kkocdko <kkocdko@163.com>
2022-07-20 01:37:31 +0800

    `units::chat` fix room not dropped

--- src/units/chat/mod.rs
+++ src/units/chat/mod.rs
@@ -1,76 +1,64 @@
 use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
-use axum::extract::{Form, Path};
+use axum::extract::Path;
 use axum::response::Html;
-use axum::response::Redirect;
 use axum::routing::MethodRouter;
 use axum::Router;
-
 use futures_util::{SinkExt, StreamExt};
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 use tokio::sync::broadcast::{self, Sender};
 
-static ROOMS: Lazy<Mutex<HashMap<u32, Arc<Room>>>> = Lazy::new(Default::default);
+static ROOMS: Lazy<Mutex<HashMap<u32, Room>>> = Lazy::new(Default::default);
 
 struct Room {
-    id: u32,
+    user_count: u32,
     channel: Sender<String>,
 }
 
-impl Room {
-    fn new(id: u32) -> Arc<Self> {
+async fn ws_handler(id: u32, ws: WebSocket) {
+    let (ch_tx, mut ch_rx) = {
         let mut rooms = ROOMS.lock().unwrap();
-        let this = rooms.entry(id).or_insert_with(|| {
-            Arc::new(Self {
-                id,
-                channel: broadcast::channel(8).0,
-            })
+        let room = rooms.entry(id).or_insert_with(|| Room {
+            user_count: 0,
+            channel: broadcast::channel(8).0,
         });
-        Arc::clone(this)
-    }
+        room.user_count += 1;
+        // broadcast::Sender::clone is based on Arc
+        (room.channel.clone(), room.channel.subscribe())
+    };
 
-    async fn ws_handler(self: Arc<Self>, ws: WebSocket) {
-        let (mut ws_tx, mut ws_rx) = ws.split();
+    let (mut ws_tx, mut ws_rx) = ws.split();
 
-        // current -> server -> others
-        let c2o_this = Arc::clone(&self);
-        let mut c2o = tokio::spawn(async move {
-            let ch_tx = &c2o_this.channel;
-            while let Some(Ok(Message::Text(v))) = ws_rx.next().await {
-                if ch_tx.send(v).is_err() {
-                    break;
-                }
+    // current -> server -> others
+    let mut c2o = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(v))) = ws_rx.next().await {
+            if ch_tx.send(v).is_err() {
+                break;
             }
-        });
+        }
+    });
 
-        // others -> server -> current
-        let o2c_this = Arc::clone(&self);
-        let mut o2c = tokio::spawn(async move {
-            let mut ch_rx = o2c_this.channel.subscribe();
-            while let Ok(v) = ch_rx.recv().await {
-                if ws_tx.send(Message::Text(v)).await.is_err() {
-                    break;
-                }
+    // others -> server -> current
+    let mut o2c = tokio::spawn(async move {
+        while let Ok(v) = ch_rx.recv().await {
+            if ws_tx.send(Message::Text(v)).await.is_err() {
+                break;
             }
-        });
+        }
+    });
 
-        // if any one of the tasks exit, abort another
-        tokio::select! {
-            _ = (&mut c2o) => o2c.abort(),
-            _ = (&mut o2c) => c2o.abort(),
-        };
-    }
-}
+    // if any one of the tasks exit, abort another
+    tokio::select! {
+        _ = (&mut c2o) => o2c.abort(),
+        _ = (&mut o2c) => c2o.abort(),
+    };
 
-impl Drop for Room {
-    fn drop(&mut self) {
-        ROOMS
-            .lock()
-            .unwrap()
-            .remove(&self.id)
-            .expect("current Room is not contained in the ROOMS");
-        println!("[chat] room {} droped", self.id);
+    let mut rooms = ROOMS.lock().unwrap();
+    let room = rooms.get_mut(&id).unwrap();
+    room.user_count -= 1;
+    if room.user_count == 0 {
+        rooms.remove(&id);
     }
 }
 
@@ -83,8 +71,7 @@ pub fn service() -> Router {
         .route(
             "/chat/ws/:room",
             MethodRouter::new().get(|Path(id): Path<u32>, u: WebSocketUpgrade| {
-                let room = Room::new(id);
-                async { u.on_upgrade(|ws| room.ws_handler(ws)) }
+                async move { u.on_upgrade(move |ws| ws_handler(id, ws)) }
                 // if not returns the response produced by `on_upgrade`, the WebSocket
                 // will receive a `ResetWithoutClosingHandshake` error
             }),
