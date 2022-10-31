b70cce20e79a80bac2a0cda96dcb9a27bd70996f
kkocdko <kkocdko@163.com>
2022-10-31 19:11:20 +0800

    `crate` the ricq is licenced under mpl now

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -7,6 +7,3 @@ pub mod paste;
 pub mod paste_next;
 pub mod qqbot;
 // pub mod record;
-
-#[cfg(not(feature = "qqbot"))]
-compile_error!("you need to prune `units::qqbot` module and `ricq` dependency manually");
