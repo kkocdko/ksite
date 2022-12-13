a0cded3d28ec2594da82cb26beab3810a6332f21
kkocdko <kkocdko@163.com>
2022-12-13 21:30:10 +0800

    `crate::database` reduce binary size

--- src/database.rs
+++ src/database.rs
@@ -1,19 +1,23 @@
 use once_cell::sync::Lazy;
-use rusqlite::Connection;
+use rusqlite::{Connection, Row, ToSql};
 use std::path::PathBuf;
 use std::sync::Mutex;
 use std::time::UNIX_EPOCH;
 
-fn get_db_file_path() -> PathBuf {
+fn file_path() -> PathBuf {
     std::env::current_exe().unwrap().with_extension("db")
 }
 
-fn load_db() -> Connection {
-    let db = Connection::open(get_db_file_path()).unwrap();
+fn load() -> Connection {
+    let db = Connection::open(file_path()).unwrap();
 
     // Optimize for Performance
     // https://www.sqlite.org/speed.html
-    // https://www.sqlite.org/pragma.html
+    // https://www.sqlite.org/pragma.html#pragma_optimize
+    // https://www.sqlite.org/pragma.html#pragma_journal_mode
+    // https://www.sqlite.org/withoutrowid.html
+
+    // SQLITE_LIMIT_LENGTH
 
     // The `WAL` mode will improve writing but slow down reading a little.
     db.pragma_update(None, "journal_mode", "WAL").unwrap();
@@ -27,17 +31,14 @@ fn load_db() -> Connection {
     db
 }
 
-/// # Use `db!()` macro instead of access directly!
-pub static DB_: Lazy<Mutex<Connection>> = Lazy::new(|| Mutex::new(load_db()));
-
 pub fn backup() {
-    let mut db = DB_.lock().unwrap();
+    let mut db = DB.lock().unwrap();
 
     // merge wal file
     db.pragma_update(None, "journal_mode", "TRUNCATE").unwrap();
 
     // shrink size
-    db.execute("VACUUM", []).unwrap();
+    db.execute_batch("VACUUM").unwrap();
 
     unsafe {
         // safety: we held the mutex in the whole period
@@ -45,52 +46,104 @@ pub fn backup() {
         db_ptr.drop_in_place(); // run Drop::drop but don't free memory
 
         std::fs::copy(
-            get_db_file_path(),
-            get_db_file_path()
-                .with_extension(format!("{}.db", UNIX_EPOCH.elapsed().unwrap().as_secs())),
+            file_path(),
+            file_path().with_extension(format!("{}.db", UNIX_EPOCH.elapsed().unwrap().as_secs())),
         )
         .unwrap();
 
-        db_ptr.write(load_db());
+        db_ptr.write(load());
     };
 }
 
+/// # Use `db!()` macro instead of access directly!
+pub mod inner_ {
+    use super::*;
+
+    pub static DB: Lazy<Mutex<Connection>> = Lazy::new(|| Mutex::new(load()));
+
+    pub fn exec_batch(sqls: &str) -> rusqlite::Result<()> {
+        let db = DB.lock().unwrap();
+        db.execute_batch(sqls)
+    }
+
+    pub fn exec_param(sql: &str, params: &[&dyn ToSql]) -> rusqlite::Result<()> {
+        let db = DB.lock().unwrap();
+        let mut stmd = db.prepare_cached(sql)?;
+        stmd.execute(params)?;
+        Ok(())
+    }
+
+    #[allow(unused)]
+    pub fn exec_param_lastid(sql: &str, params: &[&dyn ToSql]) -> rusqlite::Result<i64> {
+        let db = DB.lock().unwrap();
+        let mut stmd = db.prepare_cached(sql)?;
+        stmd.execute(params)?;
+        Ok(db.last_insert_rowid())
+    }
+
+    pub fn query_row<T, F>(sql: &str, params: &[&dyn ToSql], f: F) -> rusqlite::Result<T>
+    where
+        F: FnOnce(&Row) -> rusqlite::Result<T>,
+    {
+        let db = DB.lock().unwrap();
+        let mut stmd = db.prepare_cached(sql)?;
+        stmd.execute(params)?;
+        stmd.query_row(params, f)
+    }
+
+    pub fn query_rows<T, F>(sql: &str, params: &[&dyn ToSql], mut f: F) -> rusqlite::Result<Vec<T>>
+    where
+        F: FnMut(&Row) -> rusqlite::Result<T>,
+    {
+        let db = DB.lock().unwrap();
+        let mut stmd = db.prepare_cached(sql)?;
+        stmd.execute(params)?;
+        let mut rows = stmd.query(params)?;
+        let mut ret = Vec::new();
+        while let Ok(Some(r)) = rows.next() {
+            ret.push(f(r)?);
+        }
+        Ok(ret)
+    }
+}
+use inner_::*; // not `pub use`
+
 #[macro_export]
 macro_rules! db {
-    // simplest usage
-    ( $sql:literal ) => {{ $crate::db!($sql, []) }};
-    // execute a statement with params
+    // execute without params and cache, supports multi statement
+    ( $sqls:literal ) => {{
+        $crate::database::inner_::exec_batch(
+            $crate::strip_str!($sqls)
+        )
+    }};
+    // execute with params
     ( $sql:literal, [ $($param:expr),* ] ) => {{
-        let params = rusqlite::params![$($param),*];
-        let db = $crate::database::DB_.lock().unwrap();
-        db.prepare_cached($crate::strip_str!($sql))
-            .and_then(|mut s| s.execute(params))
+        $crate::database::inner_::exec_param(
+            $crate::strip_str!($sql),
+            rusqlite::params![$($param),*]
+        )
     }};
-    // execute a statement then returns `last_insert_rowid()`
+    // execute and returns `last_insert_rowid()`
     ( $sql:literal, [ $($param:expr),* ], & ) => {{
-        let params = rusqlite::params![$($param),*];
-        let db = $crate::database::DB_.lock().unwrap();
-        db.prepare_cached($crate::strip_str!($sql))
-            .and_then(|mut s| s.execute(params))
-            .map(|_| db.last_insert_rowid())
+        $crate::database::inner_::exec_param_lastid(
+            $crate::strip_str!($sql),
+            rusqlite::params![$($param),*]
+        )
     }};
-    // query and return the first matched row, the symbol '^' means "first" in regexp
-    ( $sql:literal, [ $($param:expr),* ], ^( $($idx:expr),* ) ) => {{
-        let params = rusqlite::params![$($param),*];
-        let db = $crate::database::DB_.lock().unwrap();
-        db.prepare_cached($crate::strip_str!($sql))
-            .and_then(|mut s| s.query_row(params, |r| Ok(( $( r.get($idx)?, )* ))))
+    // query and return the first row, the symbol '^' means "first" in regexp
+    ( $sql:literal, [ $($param:expr),* ], ^$f:expr ) => {{
+        $crate::database::inner_::query_row(
+            $crate::strip_str!($sql),
+            rusqlite::params![$($param),*],
+            $f
+        )
     }};
     // query and return all rows as `Vec<T>`
-    ( $sql:literal, [ $($param:expr),* ], ( $($idx:expr),* ) ) => {(||{
-        let params = rusqlite::params![$($param),*];
-        let mut ret = Vec::new();
-        let db = $crate::database::DB_.lock().unwrap();
-        let mut stmd = db.prepare_cached($crate::strip_str!($sql))?;
-        let mut rows = stmd.query(params)?;
-        while let Ok(Some(r)) = rows.next() {
-            ret.push(( $( r.get($idx)?, )* ));
-        }
-        std::result::Result::<_, rusqlite::Error>::Ok(ret)
-    })()};
+    ( $sql:literal, [ $($param:expr),* ], $f:expr ) => {{
+        $crate::database::inner_::query_rows(
+            $crate::strip_str!($sql),
+            rusqlite::params![$($param),*],
+            $f
+        )
+    }};
 }
