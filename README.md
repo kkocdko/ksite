13f85490d53370ec6f58549499ad392d0a8cb808
kkocdko <kkocdko@163.com>
2023-02-28 10:25:19 +0800

    magazine: Less cpu time

--- README.md
+++ README.md
@@ -8,15 +8,13 @@ All in one solution for my server.
 
 - `units::meet` supports real-time cloud record.
 
-- `crate::log` lightweight logging, use `httpdate`. provide a log viewer mode.
+- `crate::log` lightweight logging, use `httpdate`. provide a log viewer.
 
 ### 0.9.0
 
 - `crate` proactive traffic restriction.
 
 - `crate` no out-of-service updates.
-
-- `units::sandwich` cut sandwich game with co-op mode, webrtc or server forward fallback.
 
 - `units::spacehuggers` [space-huggers](https://github.com/KilledByAPixel/SpaceHuggers) game with co-op mode, webrtc or server forward fallback.
 
--- src/units/magazine/mod.rs
+++ src/units/magazine/mod.rs
@@ -6,17 +6,17 @@ use crate::{care, include_src};
 use anyhow::Result;
 use axum::body::Bytes;
 use axum::http::header::{HeaderName, HeaderValue};
-use axum::http::header::{CACHE_CONTROL, CONTENT_ENCODING, EXPIRES, REFRESH};
+use axum::http::header::{CACHE_CONTROL, EXPIRES, REFRESH};
 use axum::response::Html;
 use axum::routing::{MethodRouter, Router};
 use once_cell::sync::Lazy;
-use std::io::Write as _;
 use std::sync::Mutex;
 use std::time::{Duration, SystemTime};
 
-fn generate(mut i: &str, o: &mut String, mut limit: usize) -> Result<()> {
+fn generate(mut i: &str, o: &mut String) -> Result<()> {
+    // let mut limit = 20;
     while let Some(mut p) = i.split_once("<item>") {
-        limit -= 1;
+        // limit -= 1;
 
         *o += "\n<details>\n";
 
@@ -61,9 +61,9 @@ fn generate(mut i: &str, o: &mut String, mut limit: usize) -> Result<()> {
         *o += "</details>\n";
         i = p.1;
 
-        if limit == 0 {
-            break;
-        }
+        // if limit == 0 {
+        //     break;
+        // }
     }
     *o += "\n<br>\n";
     Ok(())
@@ -87,49 +87,69 @@ static CACHE: Lazy<Mutex<Res>> = Lazy::new(|| {
 
 async fn refresh() -> Result<()> {
     let expires = httpdate::fmt_http_date(SystemTime::now() + Duration::from_secs(3600));
-    macro_rules! rss {
-        ($p:expr) => {{
-            /*
-            https://rsshub.app
-            https://rsshub.uneasy.win
-            https://rsshub.rssforever.com
-            https://rsshub.moeyy.cn
-            https://rss.itggg.cn
-            */
-            fetch_text(concat!("https://rsshub.rssforever.com", $p))
-        }};
+    /*
+    https://rsshub.app
+    https://rsshub.uneasy.win
+    https://rsshub.rssforever.com
+    https://rsshub.moeyy.cn
+    https://rss.itggg.cn
+    http://39.107.244.19:9422
+    */
+    async fn rss(p: &str) -> Result<String, ()> {
+        let prefixs = [
+            "https://rsshub.rssforever.com",
+            "https://rsshub.feeded.xyz",
+            "http://39.107.244.19:9422",
+        ];
+        for prefix in prefixs {
+            let ret = fetch_text(&(prefix.to_string() + p)).await;
+            if let Ok(v) = ret {
+                if v.starts_with("<?xml") {
+                    return Ok(v);
+                }
+            }
+        }
+        return Err(());
     }
     let r = tokio::join!(
-        rss!("/leetcode/dailyquestion/solution/en"),
-        rss!("/bbc"),
-        rss!("/hackernews"),
-        rss!("/zhihu/daily"),
-        rss!("/oschina/news/industry"),
-        rss!("/1point3acres/post/hot3"),
-        rss!("/rustcc/jobs"),
+        // rss!("/leetcode/dailyquestion/solution/en?limit=3"),
+        rss("/bbc?limit=5"),
+        rss("/hackernews?limit=5"), // &mode=fulltext
+        rss("/zhihu/daily?limit=7"),
+        rss("/oschina/news/industry?limit=7"),
+        rss("/1point3acres/post/hot3?limit=7"),
+        rss("/rustcc/jobs?limit=4"),
     );
-    let o = tokio::task::spawn_blocking(move || {
-        let mut o = PAGE[0].to_string();
-        r.0.map(|v| generate(&v, &mut o, 5)).ok();
-        r.1.map(|v| generate(&v, &mut o, 9)).ok();
-        r.2.map(|v| generate(&v, &mut o, 9)).ok();
-        r.3.map(|v| generate(&v, &mut o, 9)).ok();
-        r.4.map(|v| generate(&v, &mut o, 9)).ok();
-        r.5.map(|v| generate(&v, &mut o, 9)).ok();
-        r.6.map(|v| generate(&v, &mut o, 5)).ok();
-        o += PAGE[1];
-        let mut enc = flate2::write::GzEncoder::new(Vec::new(), Default::default());
-        enc.write_all(o.as_bytes()).unwrap();
-        enc.finish().unwrap()
-    })
-    .await?;
+    let mut o = String::new();
+    o += PAGE[0];
+    o += &expires;
+    o += "<br><br>";
+    // let o = tokio::task::spawn_blocking(move || {
+    care!(r.0.map(|v| generate(&v, &mut o))).ok();
+    care!(r.1.map(|v| generate(&v, &mut o))).ok();
+    care!(r.2.map(|v| generate(&v, &mut o))).ok();
+    care!(r.3.map(|v| generate(&v, &mut o))).ok();
+    care!(r.4.map(|v| generate(&v, &mut o))).ok();
+    care!(r.5.map(|v| generate(&v, &mut o))).ok();
+    // care!(r.6.map(|v| generate(&v, &mut o))).ok();
+    o += PAGE[1];
+    // use std::io::Write as _;
+    // let mut enc = flate2::write::GzEncoder::new(Vec::new(), flate2::Compression::fast());
+    // enc.write_all(o.as_bytes()).unwrap();
+    // enc.finish().unwrap()
+    // })
+    // .await?;
     *CACHE.lock().unwrap() = (
         [
             (
                 EXPIRES,
                 HeaderValue::from_maybe_shared(Bytes::from(expires)).unwrap(),
             ),
-            (CONTENT_ENCODING, HeaderValue::from_static("gzip")),
+            (
+                HeaderName::from_static("server-timing"),
+                HeaderValue::from_static("missedCache"),
+            ),
+            // (CONTENT_ENCODING, HeaderValue::from_static("gzip")),
         ],
         Html(Bytes::from(o)), // `bytes::Bytes` is cheaper than `Vec<u8>` on clone
     );
