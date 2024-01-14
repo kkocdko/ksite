df99ceda3f0d5f0913e6dd9a28cfc073fc651e6a
kkocdko <kkocdko@163.com>
2024-01-15 01:50:49 +0800

    auth: Fix misauth when cookie is not the first

--- src/auth.rs
+++ src/auth.rs
@@ -3,26 +3,26 @@
 use crate::include_src;
 use crate::units::admin;
 use crate::utils::LazyLock;
-use axum::body::{Body, Bytes};
-use axum::http::header::{HeaderValue, COOKIE};
+use axum::body::Body;
+use axum::http::header::COOKIE;
 use axum::http::{Request, StatusCode};
 use axum::middleware::Next;
 use axum::response::{Html, IntoResponse, Response};
-use std::io::Write as _;
+use std::fmt::Write as _;
 
-static AUTH_COOKIE: LazyLock<HeaderValue> = LazyLock::new(|| {
-    let mut inner = Vec::new();
-    inner.extend(b"auth=");
+static AUTH_COOKIE: LazyLock<String> = LazyLock::new(|| {
+    let mut inner = String::new();
+    inner += "auth=";
     match admin::db::get("auth_key") {
-        Some(v) => inner.extend(v.as_slice()),
+        Some(v) => inner += std::str::from_utf8(&v).unwrap(),
         None => write!(&mut inner, "{:x}", rand::random::<u128>()).unwrap(),
     }
-    HeaderValue::from_maybe_shared(Bytes::from(inner)).unwrap()
+    inner
 });
 
 pub fn auth_key() -> &'static str {
     // because this is a low frequency operation
-    AUTH_COOKIE.to_str().unwrap().split_once('=').unwrap().1
+    AUTH_COOKIE.split_once('=').unwrap().1
 }
 
 pub async fn auth_layer(req: Request<Body>, next: Next) -> Response {
@@ -31,8 +31,10 @@ pub async fn auth_layer(req: Request<Body>, next: Next) -> Response {
         .headers()
         .get_all(COOKIE) // http2 allows multiple header entries with same name
         .into_iter()
-        .any(|v| v == *AUTH_COOKIE)
-    {
+        .any(|v| match v.to_str() {
+            Ok(v) if v.find(&*AUTH_COOKIE).is_some() => true,
+            _ => false,
+        }) {
         true => next.run(req).await,
         false => (StatusCode::UNAUTHORIZED, Html(AUTH_PAGE)).into_response(),
     }
