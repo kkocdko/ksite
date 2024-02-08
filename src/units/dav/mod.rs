d146e00d923c3f5c255ae44b4a6d402406f11ecc
kkocdko <kkocdko@163.com>
2024-02-08 12:28:33 +0800

    dav: Refactor and avoid gvfs error prompt

--- src/units/dav/mod.rs
+++ src/units/dav/mod.rs
@@ -43,10 +43,9 @@ mod db {
                 SELECT uid FROM dav_users WHERE auth = ?
             "};
             let mut stmd = db.prepare_cached(sql).unwrap();
-            stmd.query_row((auth.into_bytes(),), |r| {
-                Ok(String::from_utf8(r.get(0)?).unwrap())
-            })
-            .ok()
+            let v2s = |v| String::from_utf8(v).unwrap();
+            stmd.query_row((auth.into_bytes(),), |r| r.get(0).map(v2s))
+                .ok()
         })
         .await
     }
@@ -102,22 +101,17 @@ mod db {
         })
         .await
     }
-    pub async fn list_entry_meta(eid_prefix: String) -> Vec<(String, u64, u64, u64)> {
+    pub async fn list_entry_meta(eid: String, recursive: bool) -> Vec<(String, u64, u64, u64)> {
         DB.call(move |db| {
             let sql = strip_str! {"
-                SELECT eid, time, size, flag FROM dav_entries WHERE eid LIKE ? AND eid NOT LIKE ?
+                SELECT eid, time, size, flag FROM dav_entries WHERE eid LIKE ? AND (? OR eid NOT LIKE ?)
             "};
             let mut stmd = db.prepare_cached(sql).unwrap();
-            // TODO: optimize
-            let v_like = eid_prefix.to_owned() + "/%";
-            let v_not_like = eid_prefix.to_owned() + "/%/%";
-            stmd.query_map((v_like.into_bytes(), v_not_like.into_bytes()), |r| {
-                Ok((
-                    String::from_utf8(r.get(0)?).unwrap(),
-                    r.get(1)?,
-                    r.get(2)?,
-                    r.get(3)?,
-                ))
+            let mut v = eid.into_bytes();
+            v.extend(b"/%/%");
+            let v2s = |v| String::from_utf8(v).unwrap();
+            stmd.query_map((&v[..v.len() - b"/%".len()], recursive, &v), |r| {
+                Ok((v2s(r.get(0)?), r.get(1)?, r.get(2)?, r.get(3)?))
             })
             .unwrap()
             .map(|v| v.unwrap())
@@ -131,9 +125,9 @@ mod db {
                 DELETE FROM dav_entries WHERE eid = ? OR eid LIKE ?
             "};
             let mut stmd = db.prepare_cached(sql).unwrap();
-            let v_like = eid.to_owned() + "/%";
-            stmd.execute((eid.into_bytes(), v_like.into_bytes()))
-                .unwrap();
+            let mut v = eid.into_bytes();
+            v.extend(b"/%");
+            stmd.execute((&v[..v.len() - b"/%".len()], &v)).unwrap();
         })
         .await
     }
@@ -144,8 +138,8 @@ async fn dav_handler(prefix: &'static str, mut req: Request) -> anyhow::Result<R
     let method = req.method().as_str();
     if method == "OPTIONS" {
         return Ok(([
-            ("Allow", "OPTIONS, DELETE, PROPPATCH, COPY, MOVE, PROPFIND"),
-            ("Dav", "1, 2"),
+            ("allow", "OPTIONS, DELETE, PROPPATCH, COPY, MOVE, PROPFIND"),
+            ("dav", "1, 2"),
         ])
         .into_response());
     }
@@ -198,19 +192,17 @@ async fn dav_handler(prefix: &'static str, mut req: Request) -> anyhow::Result<R
                 return Err(anyhow::anyhow!("read only"));
             }
             #[allow(clippy::unnecessary_to_owned)] // false positive
-            let dest = Uri::from_maybe_shared(req.headers().get("Destination").e()?.to_owned())?;
+            let dest = Uri::from_maybe_shared(req.headers().get("destination").e()?.to_owned())?;
             let dest = dest.path().trim_start_matches(prefix);
             let dest_eid = uid + ":" + dest.trim_end_matches('/');
             if flag & db::ENTRY_DIR == 0 {
                 let data = db::get_entry_data(eid.to_owned()).await.unwrap();
                 db::set_entry(dest_eid, Bytes::from(data), time, size, flag).await;
                 if method == "MOVE" {
-                    // TODO: opti
-                    db::del_entry_recursive(eid).await;
+                    db::del_entry_recursive(eid).await; // TODO: opti
                 }
             } else {
-                // TODO
-                return Err(anyhow::anyhow!("is dir, todo"));
+                return Err(anyhow::anyhow!("is dir, todo")); // TODO
             }
             Ok(StatusCode::OK.into_response())
         }
@@ -305,78 +297,65 @@ async fn api_handler(mut req: Request) -> anyhow::Result<Response> {
         let v = req.headers_mut().remove(k);
         v.and_then(|v| Some(v.to_str().ok()?.to_owned())).e()
     };
-    Ok(match get_field("op_")?.as_str() {
+    match get_field("op_")?.as_str() {
         "signup" => {
+            return Err(anyhow::anyhow!("not allowed"));
             let uid = get_field("uid_")?;
-            if !uid
-                .as_bytes()
-                .iter()
-                .all(|&c| c.is_ascii_alphanumeric() || c == b'-' || c == b'_')
-            {
+            let valid_uid_char = |&c: &u8| c.is_ascii_alphanumeric() || c == b'-' || c == b'_';
+            if !uid.as_bytes().iter().all(valid_uid_char) {
                 return Err(anyhow::anyhow!("uid contains invalid chars"));
             }
             let auth = get_field("auth_")?;
-            db::set_user(uid.to_owned(), auth.to_owned()).await; // TODO
+            db::set_user(uid.to_owned(), auth.to_owned()).await; // TODO: reject overwrite
             let time = UNIX_EPOCH.elapsed().unwrap().as_secs();
             db::set_entry(uid.to_owned() + ":", Bytes::new(), time, 0, db::ENTRY_DIR).await;
-            StatusCode::OK.into_response()
         }
-        "trigger_flag_recursive" => {
+        "apply_flag_recursive" => {
             let eid = get_field("eid_")?;
             let not = get_field("not_").is_ok();
             let apply_dir = get_field("apply_dir_").is_ok(); // apply flag on dir, or only non-dir
             let trigger_flag: u64 = get_field("flag_")?.parse()?;
-            let mut list = Vec::new(); // certainly that std VecDeque will be better, however it's more complex inside
-            list.push(eid);
-            let mut idx = 0;
-            while idx != list.len() {
-                let eid = std::mem::take(&mut list[idx]);
-                let Some((_, _, flag)) = db::get_entry_meta(eid.to_owned()).await else {
-                    continue;
-                };
+            let auth = get_field("auth_")?;
+            let eid_uid_prefix = db::get_user_uid(auth).await.e()? + ":";
+            if !eid.starts_with(&eid_uid_prefix) {
+                return Err(anyhow::anyhow!("auth failed"));
+            }
+            let mut list = Vec::new();
+            let (_, _, flag) = db::get_entry_meta(eid.to_owned()).await.e()?;
+            list.push((eid.to_owned(), flag));
+            for (eid, _, _, flag) in db::list_entry_meta(eid, true).await {
+                list.push((eid, flag));
+            }
+            for (eid, flag) in list {
                 if flag & db::ENTRY_DIR == 0 || apply_dir {
                     let new_flag = match not {
                         true => flag & !trigger_flag,
                         false => flag | trigger_flag,
                     };
-                    db::set_entry_flag(eid.to_owned(), new_flag).await;
+                    db::set_entry_flag(eid, new_flag).await;
                 }
-                if flag & db::ENTRY_DIR != 0 {
-                    for (eid, _, _, _) in db::list_entry_meta(eid).await {
-                        list.push(eid);
-                    }
-                }
-                idx += 1;
             }
-            StatusCode::OK.into_response()
         }
         _ => return Err(anyhow::anyhow!("unknown op")),
-    })
+    };
+    Ok(StatusCode::OK.into_response())
 }
 
 pub fn service() -> Router {
     crate::utils::block_on(db::init());
     const DAV_PATH_PREFIX: &str = "/dav";
-    let api_router = axum::routing::any(|req: Request| async {
-        match care!(api_handler(req).await) {
-            Ok(res) => res,
-            Err(_) => StatusCode::BAD_REQUEST.into_response(),
-        }
-    })
-    .layer(axum::middleware::from_fn(crate::auth::auth_layer));
     let any_router = axum::routing::any(|req: Request| async {
         if req.uri().path() == DAV_PATH_PREFIX && req.method() == "GET" {
-            let mut res = Html((include_src!("page.html") as [_; 1])[0]).into_response();
-            let cache_control_value =
-                HeaderValue::from_static("max-age=600,stale-while-revalidate=31536000");
-            res.headers_mut().insert(CACHE_CONTROL, cache_control_value);
-            res
+            let mut r = Html((include_src!("page.html") as [_; 1])[0]).into_response();
+            let v = HeaderValue::from_static("max-age=600,stale-while-revalidate=31536000");
+            r.headers_mut().insert(CACHE_CONTROL, v);
+            r
         } else if req.uri().path() == DAV_PATH_PREFIX && req.method() == "POST" {
-            api_router.call(req, ()).await
-        } else if let Ok(res) = care!(dav_handler(DAV_PATH_PREFIX, req).await) {
-            res
+            let r = api_handler(req).await; // use care!() for debugging
+            r.unwrap_or_else(|_| StatusCode::BAD_REQUEST.into_response()) // in order to simplify implementation, return 400 for any error
         } else {
-            StatusCode::BAD_REQUEST.into_response()
+            let r = dav_handler(DAV_PATH_PREFIX, req).await;
+            r.unwrap_or_else(|_| StatusCode::NOT_FOUND.into_response()) // 404 here because 400 caused some client to prompt error
         }
     });
     Router::new()
